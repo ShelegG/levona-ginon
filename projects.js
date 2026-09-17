d198dbd4ab2f215d07c612bbb8dd9e8227f32a62
ShelegG <gil.sheleg@gmail.com>
2026-09-17 11:39:48 +0300

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,147 @@
+﻿// Auto-generated - 2026-09-17 11:35:00
+const PROJECTS_DATA = [
+  {
+    "name": "Aman",
+    "images": [
+      "projects/Aman/48a9d939-d93f-4c37-9664-b518ea66c248_edited.jpg",
+      "projects/Aman/7f6482a2-93ae-4e65-8db8-88c01c1b6397.jpeg",
+      "projects/Aman/ad7e9955-7c62-4dde-8d9a-1c95cc62ed42.jpeg"
+    ]
+  },
+  {
+    "name": "Amazon",
+    "images": [
+      "projects/Amazon/1.jpg",
+      "projects/Amazon/2.jpg",
+      "projects/Amazon/3.jpg",
+      "projects/Amazon/4.jpg",
+      "projects/Amazon/5.jpg"
+    ]
+  },
+  {
+    "name": "Atid",
+    "images": [
+      "projects/Atid/00d4a58d-0fe9-4a6d-81c3-2605971fb5ea.jpeg",
+      "projects/Atid/4de9e00c-d192-44f1-86b8-337bf5f81846.jpeg",
+      "projects/Atid/4e53d9b9-212f-4ed7-9045-7862916bd0a9.jpeg",
+      "projects/Atid/642da44e-b9be-4ef0-af0b-efb17dd102ea.jpeg",
+      "projects/Atid/892d8d09-445e-463a-ade8-9086f8ebd980.jpeg",
+      "projects/Atid/a9f24120-3ad3-4f78-85e9-bf718106b325.jpeg"
+    ]
+  },
+  {
+    "name": "Compugen",
+    "images": [
+      "projects/Compugen/WhatsApp Image 2016-10-31 at 09.50.49.jpeg",
+      "projects/Compugen/WhatsApp Image 2016-10-31 at 09.50.50.jpeg",
+      "projects/Compugen/WhatsApp Image 2016-10-31 at 09.50.55.jpeg",
+      "projects/Compugen/WhatsApp Image 2016-10-31 at 09.50.56.jpeg"
+    ]
+  },
+  {
+    "name": "HiBob",
+    "images": [
+      "projects/HiBob/8c80e228-6a0d-4390-af5f-93ca23189926.jpeg",
+      "projects/HiBob/e133d2d3-ef41-46d5-a93d-6b0daa954ee4.jpeg"
+    ]
+  },
+  {
+    "name": "Hubz",
+    "images": [
+      "projects/Hubz/DSCN0869.JPG",
+      "projects/Hubz/DSCN0870.JPG",
+      "projects/Hubz/DSCN0871.JPG",
+      "projects/Hubz/DSCN0873_edited.jpg",
+      "projects/Hubz/DSCN0874.JPG",
+      "projects/Hubz/DSCN0875.JPG",
+      "projects/Hubz/DSCN0876.JPG",
+      "projects/Hubz/DSCN0877.JPG",
+      "projects/Hubz/DSCN0878_edited.jpg"
+    ]
+  },
+  {
+    "name": "IronSource",
+    "images": [
+      "projects/IronSource/DSC5152.jpg",
+      "projects/IronSource/DSC5168.jpg",
+      "projects/IronSource/DSC5225.jpg",
+      "projects/IronSource/DSC5346.jpg",
+      "projects/IronSource/DSC5409.jpg",
+      "projects/IronSource/DSCN1115.JPG",
+      "projects/IronSource/DSCN1117.JPG",
+      "projects/IronSource/DSCN1119.JPG",
+      "projects/IronSource/DSCN1120.JPG",
+      "projects/IronSource/DSCN1121.JPG",
+      "projects/IronSource/DSCN1122.JPG",
+      "projects/IronSource/DSCN1123.JPG",
+      "projects/IronSource/DSCN1124.JPG",
+      "projects/IronSource/DSCN1125.JPG",
+      "projects/IronSource/DSCN1126.JPG",
+      "projects/IronSource/DSCN1129.JPG",
+      "projects/IronSource/DSCN1131.JPG",
+      "projects/IronSource/DSCN1134.JPG",
+      "projects/IronSource/DSCN1137.JPG",
+      "projects/IronSource/DSCN1141.JPG",
+      "projects/IronSource/DSCN1142.JPG",
+      "projects/IronSource/DSCN1144.JPG",
+      "projects/IronSource/DSCN1145.JPG",
+      "projects/IronSource/DSCN1154.JPG"
+    ]
+  },
+  {
+    "name": "Labs",
+    "images": [
+      "projects/Labs/IMG-20180116-WA0015.jpg",
+      "projects/Labs/IMG-20180116-WA0016.jpg",
+      "projects/Labs/IMG-20180116-WA0017.jpg",
+      "projects/Labs/IMG-20180116-WA0018.jpg",
+      "projects/Labs/IMG-20180116-WA0019.jpg",
+      "projects/Labs/IMG-20180116-WA0020.jpg",
+      "projects/Labs/IMG-20180116-WA0021.jpg",
+      "projects/Labs/IMG-20180116-WA0022.jpg",
+      "projects/Labs/IMG-20180116-WA0023.jpg",
+      "projects/Labs/LABS_TLV_©__LUZ-22.jpg",
+      "projects/Labs/LABS_TLV_©__LUZ-32.jpg",
+      "projects/Labs/LABS_TLV_©__LUZ-74.jpg"
+    ]
+  },
+  {
+    "name": "Prospera",
+    "images": [
+      "projects/Prospera/1.jpg",
+      "projects/Prospera/IMG_0020 (Large).jpg",
+      "projects/Prospera/IMG_0052 (Medium).jpg",
+      "projects/Prospera/IMG_0074 (Medium).jpg",
+      "projects/Prospera/IMG_0089 (Medium).jpg",
+      "projects/Prospera/IMG_0128 (Medium).jpg",
+      "projects/Prospera/IMG_0139 (Medium).jpg",
+      "projects/Prospera/IMG_0155 (Medium).jpg",
+      "projects/Prospera/IMG_0167 (Medium).jpg",
+      "projects/Prospera/IMG_0231 (Medium).jpg",
+      "projects/Prospera/IMG_0336 (Medium).jpg"
+    ]
+  },
+  {
+    "name": "Samsung",
+    "images": [
+      "projects/Samsung/20180601_152101.jpg",
+      "projects/Samsung/20180601_152321.jpg",
+      "projects/Samsung/20180601_152334.jpg",
+      "projects/Samsung/20180601_152402.jpg",
+      "projects/Samsung/20180601_152406.jpg",
+      "projects/Samsung/20180601_152411.jpg",
+      "projects/Samsung/20180601_152419.jpg",
+      "projects/Samsung/20180601_152425.jpg"
+    ]
+  },
+  {
+    "name": "TCM",
+    "images": [
+      "projects/TCM/PHOTO-2019-08-07-13-50-38.jpg",
+      "projects/TCM/PHOTO-2019-08-07-13-50-39 (1).jpg",
+      "projects/TCM/PHOTO-2019-08-07-13-50-39 (2).jpg",
+      "projects/TCM/PHOTO-2019-08-07-13-50-39.jpg",
+      "projects/TCM/PHOTO-2019-08-07-13-50-40.jpg"
+    ]
+  }
+];
